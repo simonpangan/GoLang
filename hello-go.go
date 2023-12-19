13670ac03a793b50d819a5c117389d5301f44e58
Simon Pangan <simonjoseph.pangan@gmail.com>
2023-12-19 22:59:41 +0800

    feat: user input

--- hello-go.go
+++ hello-go.go
@@ -1,11 +1,22 @@
 package main
 
-import(
+import (
 	"fmt"
 )
 
 var pl = fmt.Println
 
-func main() {
-	pl("Hello Go!")
-}
+// Basic Print + User Input 
+
+// func main() {
+// 	pl("What is your name?")
+
+// 	reader := bufio.NewReader(os.Stdin)
+// 	name, err := reader.ReadString('\n')
+
+// 	if err == nil {
+// 		pl("Hello, " + name)
+// 	} else {
+// 		log.Fatal(err)
+// 	}
+// }
