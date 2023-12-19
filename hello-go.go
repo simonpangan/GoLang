63eb6dc2703e76b2add5b4be33152bdacc2c685a
Simon Pangan <simonjoseph.pangan@gmail.com>
2023-12-19 23:59:54 +0800

    feat: Casting

--- hello-go.go
+++ hello-go.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"reflect"
+	"strconv"
 )
 
 var pl = fmt.Println
@@ -52,3 +53,29 @@ var pl = fmt.Println
 // 	pl(reflect.TypeOf("Hello"))
 // 	pl(reflect.TypeOf("😌"))
 // }
+
+// Casting
+
+func main() {
+	cV1 := 3.14
+	cV2 := int(cV1)
+
+	pl(cV2)
+
+	cv3 := "500000000"
+	cv4, err := strconv.Atoi(cv3)
+
+	pl(cv4, err, reflect.TypeOf(cv4))
+
+	cv5 := 500000000
+	cv6 := strconv.Itoa(cv5)
+	pl(cv6, reflect.TypeOf(cv6))
+
+	cv7 := "3.14"
+
+	if cv8, err := strconv.ParseFloat(cv7, 64); err == nil {
+		pl(cv8, reflect.TypeOf(cv8))
+	}
+
+	cv9 := fmt.Sprintf("%f", 3.14)
+}
