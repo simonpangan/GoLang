c9cedc61053ee417e9c074f7e318503affc4b4ca
Simon Pangan <simonjoseph.pangan@gmail.com>
2023-12-20 18:59:59 +0800

    feat: formatted print

--- hello-go.go
+++ hello-go.go
@@ -215,3 +215,30 @@ func mathematics() {
 	// Atan, Asinh, Acosh, Atanh, Atan2, Cosh, Sinh, Sincos
 	// Htpot
 }
+
+// ----- FORMATTED PRINT -----
+
+func formatted() {
+	// Go has its own version of C's printf
+	// %d : Integer
+	// %c : Character
+	// %f : Float
+	// %t : Boolean
+	// %s : String
+	// %o : Base 8
+	// %x : Base 16
+	// %v : Guesses based on data type
+	// %T : Type of supplied value
+
+	fmt.Printf("%s %d %c %f %t %o %x\n", "Stuff", 1, 'A',
+		3.14, true, 1, 1)
+
+	// Float formatting
+	fmt.Printf("%9f\n", 3.14)      // Width 9
+	fmt.Printf("%.2f\n", 3.141592) // Decimal precision 2
+	fmt.Printf("%9.f\n", 3.141592) // Width 9 no precision
+
+	// Sprintf returns a formatted string instead of printing
+	sp1 := fmt.Sprintf("%9.f\n", 3.141592)
+	pl(sp1)
+}
