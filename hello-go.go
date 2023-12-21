b5d4203997fe9d567b2ec9f10dbb22b84579435c
Simon Pangan <simonjoseph.pangan@gmail.com>
2023-12-21 12:39:39 +0800

    feat: pointers

--- hello-go.go
+++ hello-go.go
@@ -471,6 +471,7 @@ func functionss() {
 	// Variadic Function
 	pl("Unknown Sum :", getSum2(1, 2, 3, 4))
 
+	// Pass an array to a function by value
 	vArr := []int{1, 2, 3, 4}
 	pl("Array Sum :", getArraySum(vArr))
 
@@ -530,4 +531,63 @@ func getArraySum(arr []int) int {
 func changeVal(f3 int) int {
 	f3 += 1
 	return f3
+}
+
+// ----- POINTERS -----
+
+func pointers() {
+	// You can pass by reference with the &
+	// (Address of Operator)
+	// Print amount and address for amount in memory
+	f4 := 10
+	pl("f4 :", f4)
+	pl("f4 Address :", &f4)
+
+	// Store a pointer (Pointer to int)
+	var f4Ptr *int = &f4
+	pl("f42 Address :", f4Ptr)
+
+	// Print value at pointer
+	pl("f4 Value :", *f4Ptr)
+
+	// Assign value using pointer
+	*f4Ptr = 11
+	pl("f4 Value :", *f4Ptr)
+
+	// Change value in function
+	pl("f4 before function :", f4)
+	changeVal2(&f4)
+	pl("f4 after function :", f4)
+
+	// Pass an array by reference
+	pArr := [4]int{1, 2, 3, 4}
+	dblArrVals(&pArr)
+	pl(pArr)
+
+	// Passing a slice to a function works just
+	// like when using variadic functions
+	// Just add ... after the slice when passing
+	iSlice := []float64{11, 13, 17}
+	fmt.Printf("Average : %.3f\n", getAverage(iSlice...))
+}
+
+func changeVal2(myPtr *int) {
+	*myPtr = 12
+}
+
+// Receives array by reference and doubles values
+func dblArrVals(arr *[4]int) {
+	for x := 0; x < 4; x++ {
+		arr[x] *= 2
+	}
+}
+
+func getAverage(nums ...float64) float64 {
+	var sum float64 = 0.0
+	var numSize float64 = float64(len(nums))
+
+	for _, val := range nums {
+		sum += val
+	}
+	return (sum / numSize)
 }
