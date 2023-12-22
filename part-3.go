f541920a9bee75f547aa525032134332d908f6bf
Simon Pangan <simonjoseph.pangan@gmail.com>
2023-12-22 17:58:37 +0800

    feat: Channels

--- part-3.go
+++ part-3.go
@@ -43,3 +43,44 @@ func printTo10() {
 		pl("Func 2 :", i)
 	}
 }
+
+// ----- CHANNELS -----
+
+func channels() {
+	// You can have goroutines communicate by
+	// using channels. The sending goroutine
+	// also makes sure the receiving goroutine
+	// receives the value before it attempts
+	// to use it
+
+	// Create a channel : Only carries values of
+	// 1 type
+	channel1 := make(chan int)
+	channel2 := make(chan int)
+
+	go nums1(channel1)
+	go nums2(channel2)
+
+	pl(<-channel1)
+	pl(<-channel1)
+	pl(<-channel1)
+	pl(<-channel2)
+	pl(<-channel2)
+	pl(<-channel2)
+}
+
+// These functions will print in order using
+// channels
+// Func receives a channel and then sends values
+// over channels once each time it is called
+func nums1(channel chan int) {
+	channel <- 1
+	channel <- 2
+	channel <- 3
+}
+
+func nums2(channel chan int) {
+	channel <- 4
+	channel <- 5
+	channel <- 6
+}
