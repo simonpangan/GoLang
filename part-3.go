a18721d8c56d0b5823dc85ca5e3642a2a59cdc62
Simon Pangan <simonjoseph.pangan@gmail.com>
2023-12-23 00:06:59 +0800

    feat: Web App

--- part-3.go
+++ part-3.go
@@ -235,3 +235,9 @@ func test() {
 	// Type: go mod init app2
 	// Run Tests : go test -v
 }
+
+// ----- GO ON THE WEB -----
+// Our Go apps can run in the browser
+// Create directory webapp with webapp.go
+// Run our app with go run webapp.go
+// Ctrl + c to stop server
