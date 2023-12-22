46df25f34ff257d90bf076ce73292af214711e51
Simon Pangan <simonjoseph.pangan@gmail.com>
2023-12-22 20:43:28 +0800

    feat: Closures

--- part-3.go
+++ part-3.go
@@ -129,3 +129,36 @@ func (a *Account) Withdraw(v int) {
 		a.balance -= v
 	}
 }
+
+// ----- CLOSURES -----
+
+func closures() {
+	// Closures are functions that don't have to be
+	// associated with an identifier (Anonymous)
+
+	// Create a closure that sums values
+	intSum := func(x, y int) int {
+		return x + y
+	}
+
+	pl("5 + 4 =", intSum(5, 4))
+
+	// Closures can change values outside the function
+	samp1 := 1
+	changeVar := func() { samp1 += 1 }
+	changeVar()
+	pl("samp1 =", samp1)
+
+	// Pass a function to a function
+	useFunc(sumVals, 5, 8)
+}
+
+// ----- CLOSURES -----
+// Pass a function to a function
+func useFunc(f func(int, int) int, x, y int) {
+	pl("Answer :", (f(x, y)))
+}
+
+func sumVals(x, y int) int {
+	return x + y
+}
